d206375f0adc0628fa087478b77919d4a5db689e
DeAndre Cook <96674521+dcookcode11010@users.noreply.github.com>
2022-04-10 17:55:33 -0400

    Add files via upload

---
+++ LinkedListCars/src/SportCars/Cars.java
@@ -0,0 +1,18 @@
+package SportCars;
+
+import java.util.LinkedList;
+
+public class Cars {
+	//Just demonstrating linked list
+	public static void main(String[] args)
+	{
+		LinkedList<String> cars = new LinkedList<String>();
+		
+		cars.add("Mercedes");
+		cars.add("BMW");
+		cars.add("Porsche");
+		 
+		System.out.println(cars);
+	}
+
+}
